eae930c33f51135fe5e90f9949de53b3afb69c33
youdias <youdiasGray@outlook.com>
2024-04-17 15:09:49 +0800

    update,生成vs解决方案项目文件，可以直接编译通过。

---
+++ lua-5.2.4/src/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Home/code/git/Lua_2_2_4/lua-5.2.4/src
+# Build directory: D:/Home/code/git/Lua_2_2_4/lua-5.2.4/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
